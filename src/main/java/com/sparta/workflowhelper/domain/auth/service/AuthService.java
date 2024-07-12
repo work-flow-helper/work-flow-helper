c46dfa6ba3b5952d909998b48406197a3087e1dc
Jaeseok Lee <wotjr4017@gmail.com>
2024-07-12 20:35:30 +0900

    🎉 FEAT: 로그인 기능 구현

--- src/main/java/com/sparta/workflowhelper/domain/auth/service/AuthService.java
+++ src/main/java/com/sparta/workflowhelper/domain/auth/service/AuthService.java
@@ -3,34 +3,42 @@ package com.sparta.workflowhelper.domain.auth.service;
 import com.sparta.workflowhelper.domain.auth.adapter.AuthAdapter;
 import com.sparta.workflowhelper.domain.auth.dto.AuthRequestDto;
 import com.sparta.workflowhelper.domain.auth.dto.AuthResponseDto;
-import com.sparta.workflowhelper.domain.user.adapter.UserAdapter;
+import com.sparta.workflowhelper.domain.auth.dto.LoginRequestDto;
 import com.sparta.workflowhelper.domain.user.entity.User;
-import com.sparta.workflowhelper.domain.user.repository.UserRepository;
 import com.sparta.workflowhelper.global.common.enums.UserRole;
 import com.sparta.workflowhelper.global.exception.customexceptions.InvalidAdminCodeException;
 import com.sparta.workflowhelper.global.exception.customexceptions.UserDuplicateException;
 import com.sparta.workflowhelper.global.exception.errorcodes.InvalidAdminCodeErrorCode;
 import com.sparta.workflowhelper.global.exception.errorcodes.UserDuplicateErrorCode;
+import com.sparta.workflowhelper.global.jwt.JwtProvider;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-public class AuthService{
+public class AuthService {
 
     private final AuthAdapter authAdapter;
     private final PasswordEncoder passwordEncoder;
-    private final UserRepository userRepository;
+    private final AuthenticationManager authenticationManager;
+    private final JwtProvider jwtProvider;
 
     // 환경 변수 불러오기
     @Value("${admin.code}")
     private String adminCode;
 
+    @Transactional
     public AuthResponseDto signUp(AuthRequestDto requestDto) {
         // 중복된 username 확인
-        if(authAdapter.existsByUsername(requestDto.getUsername())) {
+        if (authAdapter.existsByUsername(requestDto.getUsername())) {
             throw new UserDuplicateException(UserDuplicateErrorCode.DUPLICATE_USERNAME.getMessage());
         }
 
@@ -50,5 +58,30 @@ public class AuthService{
         User savedUser = authAdapter.save(user);
 
         return AuthResponseDto.of(savedUser.getId(), savedUser.getNickname(), savedUser.getEmail());
+    }
+
+    @Transactional
+    public void logIn(LoginRequestDto requestDto, HttpServletResponse response) {
+
+        User user = authAdapter.findByUsername(requestDto.getUsername());
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(requestDto.getUsername(), requestDto.getPassword())
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        UserRole role = user.getUserRole();
+
+        String jwtAccessToken = jwtProvider.createdAccessToken(requestDto.getUsername(), role);
+
+        String jwtRefreshToken = jwtProvider.createdRefreshToken();
+
+        user.updateRefreshToken(jwtRefreshToken);
+        authAdapter.save(user);
+
+        response.setHeader(JwtProvider.ACCESS_HEADER, jwtAccessToken);
+        response.setHeader(JwtProvider.REFRESH_HEADER, jwtRefreshToken);
+
     }
 }
